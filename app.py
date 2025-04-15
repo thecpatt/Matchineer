b02e9366c98d568bb58455c5f97ed3a6ea96e1e8
Carson <pattersoncw@appstate.edu>
2025-04-15 12:44:06 -0400

    modified:   app.py

--- app.py
+++ app.py
@@ -492,6 +492,5 @@ def user_matching(user_data):
 
 ## RUN IT
 
-if __name__ == "__main__":
-    
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    app.run()
